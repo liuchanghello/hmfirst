dd6c02d90dcd26297ff9c66cccc0650f7f444407
sb <sb@qq.com>
2019-10-08 18:05:00 +0800

    添加路由

--- src/router.js
+++ src/router.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// 导入login/home
+import Login from '@/views/login/index.vue'
+import Home from '@/views/home'
+
 Vue.use(Router)
 
-export default new Router()
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    }, {
+      path: '/login',
+      component: Login
+    }, {
+      path: '/home',
+      component: Home
+    }]
+})
